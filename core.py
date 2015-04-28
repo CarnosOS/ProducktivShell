277744b2f457b87e819722b1581a0905d6badca0
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-04-28 08:38:22 -0400

    Now

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
 import string, sys, sqlite3, cmd, dropbox, webbrowser
-
+from Lib import archersys
 class ProducktivShellRolodex(cmd.Cmd):
     def __init__(self):
          
@@ -13,12 +13,29 @@ class ProducktivShellRolodex(cmd.Cmd):
          self.cursor = self.db.cursor()
          self.prompt =  self.database_name + " -> ProducktivShell:"
     def do_CreateDB(self,args):
-         """ Creates a new ArcherDB Database (MySQl or SQLite3 for Python) """
+         """ Creates a new ArcherDB Database (MySQl or SQLite3 for Python) 
+        
+          Define  do_CreateDB
+          Ask  Contact Address Book Name:
+          Save_To_DB
+          Create Tables -> contacts(family, friends, etc.)
+          ResetDictionary """
          self.db = sqlite3.connect(self.database_name + ".db")
          self.cursor = self.db.cursor()
-         self.cursor.execute("create table family (id text, first_name text,last_name text,phone text,email text)")
+         try:
+             self.cursor.execute("create table family (id text, first_name text,last_name text,phone text,email text)")
+         except sqlite3.OperationalError:
+              print("The family contacts group already exists.")
+         try:
+             self.cursor.execute("create table friend (id text, first_name text,last_name text,phone text,email text)")
+         except sqlite3.OperationalError:
+             print("The Friends Contact Group already exists.")
+   
     def do_NewContact(self,arg):
-          """Creates a new contact for the picking!"""
+          """Creates a record for the contacts database
+             Define   NewContact
+             New_contact = Ask Name: Phone Number: Email:
+             SaveNewDBRecord New_Contact """
           self.id += 1
           first_name = input("First Name")
           last_name = input("Last Name:")
@@ -36,6 +53,13 @@ class ProducktivShellRolodex(cmd.Cmd):
           print("Email: " + record[3])
           print("="*41)
     def do_EditContact(self,arg):
+          """ Edits a Record
+          Define     EditContact
+          Edit_Contact = Select_Record Ask Index
+          Ask RECORD_EDIT_PART
+          Ask(Edit) RECORD_EDIT_PART 
+          Save_To_DB (Ans)"""
+
           field = input("Field:")
           edit_id = int(input("ID to Edit:"))
           query = "UPDATE " + input("Contact Type:") + " SET "+field+"=\""+ input(field) + "\" WHERE id="+str(int(input("ID:")))
@@ -43,14 +67,18 @@ class ProducktivShellRolodex(cmd.Cmd):
          
           self.cursor.execute(query)
     def do_DeleteContact(self,arg):
+          """Deletes a Contact
+          Ask Select_Record
+          DELETE_RECORD Ans
+          SaveChanges"""
           field = input("Search Field:")
           value = input("Value:")
           print("Deleting...")
           self.cursor.execute("DELETE FROM " + input("Contact Type:") + " WHERE " + field + "=" + value)
           print("Deleted Record.")
     def do_InitDropbox(self,arg):
-          app_key = 'brwekpcno93vtpz'
-          app_secret = 'np5coz7g4nzp9og'
+          app_key = archersys.ARCHERSYS_FOR_DROPBOX_KEY
+          app_secret = archersys.ARCHERSYS_FOR_DROPBOX_SECRET
 
 
           flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
@@ -77,10 +105,64 @@ class ProducktivShellRolodex(cmd.Cmd):
           out.close()
           print(metadata)
     def  do_Save(self, arg): 
+          """ Saves the Database and all of its contents 
+          Define Save
+          Save_To_Dropbox DB """
+          self.db.commit()
           f = open(self.database_name + ".db","rb")    
           response = self.client.put_file(self.database_name + ".db", f)
           print("uploaded:", response)
-   
-contacts = core.ProducktivShellRolodex()
-contacts.cmdloop()
+    def  do_ReadOneContact(self, arg):
+          """Reads One Contact selected by an index you specify"""
+          self.cursor.execute("SELECT * FROM "+input("Contact Type:") + " WHERE id="+input("ID:"))
+          record = self.cursor.fetchone()
+          print("="*41)
+          print("Name: " + record[1] + " " + record[2])
+          print("="*41)
+          print("Phone Number:" + record[3])
+          print("="*41)
+          print("Email: " + record[4])
+          print("="*41)
+          
+    def  do_ReadAllContacts(self, arg):
+          """Reads all of the Contacts."""
+          for row in self.cursor.execute("SELECT * FROM " + input("Contact Type:")):
+             print("="*41)
+             print("Contact "+row[0])
+             print("="*41)
+             print("Name: " + row[1] + " " + row[2])
+             print("="*41)
+             print("Phone Number:" + row[3])
+             print("="*41)
+             print("Email: " + row[4])
+             print("="*41)
+    def  do_Load(self, arg): 
+          """ Loads a  SQLite Database
+          Define Load
+          Load_From_ArcherVMFS Ask File:"""
+
+          self.database_name = arg
+          self.db = sqlite3.connect(self.database_name + ".db")
+          self.cursor = self.db.cursor()
+    def  help_overview(self):
+          print("APPLICATION TITLE:  ProducktivShell\n")
+          print("="*41)  
+          print("CREATOR: Weldon Henson\n")
+          print("="*41)
+
+          print("The ProducktivShell Application is a cli application that deals with not just cloud services,\n") 
+          print("but also has a special part of this python package for the contacts list.\n")
+          print("ProdSh (ProducktivShell for short) uses Dropbox to save contacts using the pickle module,\n")
+          print(" the Dropbox core API, and the Evernote API is used to save the contacts to an Evernote note, and more.\n")
+
+    def  do_EOF(self, args):
+          """Quits this program"""
+          self.db.commit()
+          sys.exit()
+          
+        
+
+
+          
+ 
          
